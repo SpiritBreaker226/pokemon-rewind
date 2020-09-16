28b9235ab46c50e3cdfdfe8d3e1e0ceae65a2e3d
Jason Stathopulos <jstathopulos@gmail.com>
2020-09-16 18:27:52 -0400

    Add Associations Between Attack, Card and Type

--- server/app/models/type.rb
+++ server/app/models/type.rb
@@ -1,5 +1,6 @@
 class Type < ApplicationRecord
   has_and_belongs_to_many :costs_to_attack, class_name: 'Attack'
+  has_and_belongs_to_many :cards
 
   validates :name, presence: true, uniqueness: true
 end
