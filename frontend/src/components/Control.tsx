0939d78558ff0f1515c5dfd823a38faff2c73637
Jason Stathopulos <jstathopulos@gmail.com>
2020-09-18 16:30:27 -0400

    Update Styling in Control Component
    Update the styling in the control component so that there is the spacing
    between the components.

--- frontend/src/components/Control.tsx
+++ frontend/src/components/Control.tsx
@@ -1,35 +1,17 @@
 import React, { useContext } from 'react'
 
-import { Button } from '@material-ui/core'
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import { Grid, Button } from '@material-ui/core'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
 
 import { Types } from '../types/Actions'
 
 import { AppContext } from '../contexts/AppContext'
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
     control: {
       textAlign: 'right',
       padding: '0.12em 0 0 0',
-    },
-
-    link: {
-      backgroundColor: '#0078fd',
-      padding: '0.5em 0.8em',
-      textDecoration: 'none',
-      color: '#fff',
-      cursor: 'pointer',
-      textAlign: 'center',
-      fontWeight: 500,
-      lineHeight: '32px',
-      border: 'none',
-      borderRadius: '4px',
-
-      '&:hover': {
-        backgroundColor: '#3290fc',
-        textDecoration: 'none',
-      },
     },
   })
 )
@@ -61,23 +43,28 @@ const Control = () => {
 
   return (
     <section className={classes.control}>
-      <Button
-        variant="outlined"
-        data-testid="control_backup_create"
-        color="primary"
-        onClick={handleClickBackUp}
-      >
-        Create Backup
-      </Button>
-
-      <Button
-        variant="outlined"
-        data-testid="control_backup_purge"
-        color="primary"
-        onClick={handleClickBackUpPurge}
-      >
-        Purge Backup
-      </Button>
+      <Grid container>
+        <Grid item xs={6}>
+          <Button
+            variant="outlined"
+            data-testid="control_backup_create"
+            color="primary"
+            onClick={handleClickBackUp}
+          >
+            Create Backup
+          </Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Button
+            variant="outlined"
+            data-testid="control_backup_purge"
+            color="secondary"
+            onClick={handleClickBackUpPurge}
+          >
+            Purge Backup
+          </Button>
+        </Grid>
+      </Grid>
     </section>
   )
 }
